f71f8e62e8640b8a48f0308b5909d5e16b709f28
victorhfsilva <victorhfsilva@protonmail.com>
2022-09-20 11:27:44 -0300

    Edição do arquivo PartyPeople.py

--- POO/PartyPeople.py
+++ POO/PartyPeople.py
@@ -1,15 +1,20 @@
 class PartyPeople:
     
-    persons = 0
+    #In python there aren't private attributes or methods, an underscore is used to indicate this attribute shouldn't be accessed
+    _persons = 0
     
     def party(self):
-        self.persons = self.persons + 1
-        print("So far ",self.persons," persons.")
-
+        self._persons = self._persons + 1
+        print("So far ",self._persons," persons.")
+    
+    #Two underscores places the name of the class before the method or attribute to avoid overwriting 
+    def __hello_people(self):
+        print("Hello people!")
+        
     #Constructor
     def __init__(self, persons):
         print("The object was created.")
-        self.persons = persons
+        self._persons = persons
     
     #Destructor
     def __del__(self):
@@ -17,7 +22,7 @@ class PartyPeople:
         
     #To-String
     def __repr__(self):
-    	return "The party is rocking. There are {} persons.".format(self.persons)
+    	return "The party is rocking. There are {} persons.".format(self._persons)
     	
 
 #Inheritance
@@ -26,17 +31,20 @@ class PartyGirls(PartyPeople):
     girls_percentage = 0
     girls = 0
     
+    def __hello_people(self):
+        print("Hello girls!")
+    
     def party(self):
         
-        self.persons = self.persons + 1
-        self.girls = self.girls_percentage * self.persons
+        self._persons = self._persons + 1
+        self.girls = self.girls_percentage * self._persons
         
-        print("So far ",self.persons," persons. And ",self.girls," girls.")
+        print("So far ",self._persons," persons. And ",self.girls," girls.")
         is_whole(self.girls)
     
     def __init__(self,girls_percentage,persons):
         
-        self.persons = persons
+        self._persons = persons
         self.girls_percentage = girls_percentage
         
         if girls_percentage > 1 or girls_percentage < 0:
@@ -47,7 +55,7 @@ class PartyGirls(PartyPeople):
                 print("This party is wild!")
             else:
                 print("This party sucks!")
-            self.girls = girls_percentage * self.persons
+            self.girls = girls_percentage * self._persons
             print("So far ",self.girls," girls.")
             is_whole(self.girls)
 
@@ -66,6 +74,12 @@ party2.party()
 party3 = PartyGirls(0.72,10)
 party3.party()
 print(party3.girls)
+
+print(party1)
+print(party3)
+
+party3._PartyPeople__hello_people()
+party3._PartyGirls__hello_people()
 
 print(dir(party2))
 print(dir(party3))
